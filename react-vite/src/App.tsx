e402b1b7b1321d8bc2ec69cb7dfe7a545796296c
Sơn Nguyễn <son@sonng.dev>
2022-12-15 16:09:07 +0000

    feat: Visualize Dijkstra algorithm

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,16 +1,24 @@
 import { useMemo, useState } from 'react';
-import './App.css'
+import produce from 'immer';
 import Node, { NodeType } from './Node';
+import { Node as NodeDS } from '../../practical/data-structures/Node';
+import performDijkstraAlgorithm from '../../practical/algorithms/Dijkstra';
+import './App.css'
 
 type NodeState = {
   isVisited: boolean;
   isOnPath: boolean;
 }
 
+type NodeData = {
+  row: number;
+  col: number;
+}
+
 const NUM_ROWS = 20;
 const NUM_COLS = 50;
-const START_NODE = { row: 9, col: 10 };
-const END_NODE = { row: 9, col: 40 }
+const START_NODE = { row: 9, col: 20 };
+const END_NODE = { row: 9, col: 30 }
 
 function App() {
   const initialNodeStates = useMemo(() => {
@@ -18,13 +26,70 @@ function App() {
     for (let i = 0; i < NUM_ROWS; i++) {
       const row: NodeState[] = [];
       for (let j = 0; j < NUM_COLS; j++) {
-        row.push({ isVisited: false, isOnPath: true });
+        row.push({ isVisited: false, isOnPath: false });
       }
       states.push(row);
     }
     return states;
   }, [NUM_ROWS, NUM_COLS]);
   const [nodeStates, setNodeStates] = useState(initialNodeStates);
+
+  const findShortestPath = () => {
+    setNodeStates(initialNodeStates);
+
+    let grid: NodeDS<NodeData>[][] = [];
+    let startNode: NodeDS<NodeData> | null = null;
+    let endNode: NodeDS<NodeData> | null = null;
+
+    for (let i = 0; i < NUM_ROWS; i++) {
+      const row: NodeDS<NodeData>[] = [];
+      for (let j = 0; j < NUM_COLS; j++) {
+        const node = new NodeDS({ row: i, col: j });
+        if (j > 0) {
+          const leftNode = row[j - 1];
+          node.neighbors.push(leftNode);
+          leftNode.neighbors.push(node);
+        }
+        if (i > 0) {
+          const topNode = grid[i - 1][j];
+          node.neighbors.push(topNode);
+          topNode.neighbors.push(node);
+        }
+
+        row.push(node);
+
+        if (i === START_NODE.row && j === START_NODE.col) {
+          startNode = node;
+        } else if (i === END_NODE.row && j === END_NODE.col) {
+          endNode = node;
+        }
+      }
+      grid.push(row);
+    }
+    if (!startNode || !endNode) {
+      return;
+    }
+
+    const { visitedNodes, shortestPath } = performDijkstraAlgorithm(grid.flat(), startNode, endNode);
+
+    for (let i = 0; i < visitedNodes.length; i++) {
+      setTimeout(() => {
+        const { data: { row, col } } = visitedNodes[i];
+        setNodeStates(produce((draft) => {
+          draft[row][col].isVisited = true;
+        }));
+      }, i * 10);
+    }
+
+    for (let i = 0; i < shortestPath.length; i++) {
+      setTimeout(() => {
+        const { data: { row, col } } = shortestPath[i];
+        setNodeStates(produce((draft) => {
+          draft[row][col].isOnPath = true;
+        }));
+      }, (visitedNodes.length + i) * 10);
+    }
+  };
 
   return (
     <div className="App">
@@ -39,18 +104,27 @@ function App() {
             } else {
               type = NodeType.MIDDLE;
             }
+            const { isVisited, isOnPath } = node;
 
             return (
               <Node
                 key={`node-${rowIndex}-${colIndex}`}
                 type={type}
-                isVisited={rowIndex === 3}
-                isOnPath={colIndex === 5}
+                isVisited={isVisited}
+                isOnPath={isOnPath}
               />
             )
           })
         ))}
       </div>
+
+      <button
+        type="button"
+        className="action"
+        onClick={findShortestPath}
+      >
+        Find shortest path
+      </button>
     </div>
   )
 }
