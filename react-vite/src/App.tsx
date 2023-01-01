2bfa6859759b90aa551afbf309e7b2745e63faa2
Sơn Nguyễn <son@sonng.dev>
2023-01-01 14:59:36 +0000

    feat: Adjust grid styling

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -170,23 +170,27 @@ function App() {
         <div className="legends">
           <div className="legend-group">
             <div className="node start"></div>
-            <span>Start node</span>
+            <span className="label">Start node</span>
           </div>
           <div className="legend-group">
             <div className="node end"></div>
-            <span>End node</span>
+            <span className="label">End node</span>
           </div>
           <div className="legend-group">
             <div className="node"></div>
-            <span>Unvisited node</span>
+            <span className="label">Unvisited node</span>
           </div>
           <div className="legend-group">
             <div className="node visited"></div>
-            <span>Visited node</span>
+            <span className="label">Visited node</span>
           </div>
           <div className="legend-group">
             <div className="node on-path"></div>
-            <span>Shortest-path node</span>
+            <span className="label">Shortest-path node</span>
+          </div>
+          <div className="legend-group">
+            <div className="node wall"></div>
+            <span className="label">Wall node</span>
           </div>
         </div>
 
